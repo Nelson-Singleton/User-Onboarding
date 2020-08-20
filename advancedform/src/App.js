ea655c41f2d96d895c91c46b5c752a8bc1bbaa61
nelson-singleton <nsingleton@ymail.com>
2020-08-19 22:52:05 -0500

    Still working on app.js. It currently compiles successfully. Used code from tk/lecture

--- advancedform/src/App.js
+++ advancedform/src/App.js
@@ -6,8 +6,8 @@ import * as yup from 'yup'
 export default function App() {
 
 //initial form values
-const initialValues = {
-username: '',
+const initialFormValues = {
+name: '',
 email: '',
 password: '',
 termsOfService: {acceptTerms: false,},
@@ -17,17 +17,114 @@ const initialDisabled = true
 
 //initial error values
 const initialErrors = {
-  username: '',
+  name: '',
   email: '',
   password: '',
 }
 
-//slices of state
-const {formData, setFormData} = useState() 
+const initialSignUps = []
 
+
+
+
+//event handler
+const submitForm = e => {
+  e.preventDefault();
+  console.log("submitted");
+};
+
+//slices of state
+const {formData, setFormData} = useState(initialFormValues) 
+const [errors, setErrors] = useState(initialErrors)
+const [disabled, setDisabled] = useState(initialDisabled)
+const [signUps, setSignUps] = useState(initialSignUps)    
+
+//form schema
+const formSchema = yup.object().shape({
+  name: yup
+    .string()
+    .email("Must be a valid email address.")
+    .required("Must include email address."),
+  email: yup
+    .string()
+    .min(6, "Passwords must be at least 6 characters long.")
+    .required("Password is Required"),
+  password: yup
+    .string()
+    .min(6, "Passwords must be at least 6 characters long.")
+    .required("Password is Required"),
+  termsOfService: yup
+    .boolean()
+    .oneOf([true], "You must accept Terms and Conditions")
+    
+});
+
+//post request
+const postNewSignUp = newSignUp => {
+
+  axios.post('https://reqres.in/api/users', newSignUp)
+    .then(res => {
+      
+      setSignUps([...signUps, res.data])
+    })
+    .catch(err => {
+      debugger
+    })
+    .finally(() => {
+      setFormData(initialFormValues)
+    })
+}
+
+//form state
+const checkboxChange = (name, isChecked) => {
+
+  setFormData({
+    ...formData,
+    termsOfService: {
+      ...initialFormValues.termsOfService,
+      [name]: isChecked,
+    }
+  })
+}
+
+//submit button function
+const submit = () => {
+  const newFriend = {
+    name: formData.username.trim(),
+    email: formData.email.trim(),
+    password: formData.role,
+    // 🔥 STEP 8- WHAT ABOUT HOBBIES?
+    termsOfService: Object.keys(formData.termsOfService).filter(terms => formData.termsOfService[terms]),
+  }
+  // 🔥 STEP 9- POST NEW FRIEND USING HELPER
+  postNewFriend(newFriend)
+}
 
 return(
-  <div></div>
+  <div>
+    <form>
+      <label>
+        Name:
+        <input type="text" onChange={event => (event)}></input>
+      </label>
+      <label>
+        E-Mail:
+        <input type="email" onChange={event => (event)}></input>
+      </label>
+      <label>
+        Password:
+        <input type="text" onChange={event => (event)}></input>
+      </label>
+      <label>
+       Do you accept the terms and conditions? Check for yes.:
+        <input
+          name="terms"
+          type="checkbox"
+          checked={false}
+          onChange />
+      </label>
+    </form>
+  </div>
 )
 
 
