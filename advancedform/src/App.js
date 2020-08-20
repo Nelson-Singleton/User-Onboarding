6be4f35dfcd5efaa8f9bb9923f6cfdde643824c4
nelson-singleton <nsingleton@ymail.com>
2020-08-20 00:51:24 -0500

    Created initial variables and states

--- advancedform/src/App.js
+++ advancedform/src/App.js
@@ -4,7 +4,34 @@ import axios from 'axios'
 import * as yup from 'yup'
 import Form from './Form'
 
+  //initial variables *************************************
+  const initialFormValues = {
+    name: "name",
+    email: "email",
+    password: "password",
+    terms: false,
+  }
+
+  const initialFormError = {
+    name: "",
+    email: "",
+    password: "",
+
+  }
+
+  const initialUserList = []
+  const initialDisabled = true
+
 function App() {
+  
+  //states*********************************************
+const [userList, setUserList] = useState(initialUserList)
+const [formValues, setFormValues] = useState(initialFormValues)
+const [formErrors, setFormErrors] = useState(initialFormError)
+const [disabled, setDisabled] = useState(initialDisabled)
+
+
+
 
 
 
