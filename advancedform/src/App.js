fa0c89a795cc161eafca53d28f6991e790152e5b
nelson-singleton <nsingleton@ymail.com>
2020-08-19 20:20:11 -0500

    Set up app.js. Imported necessary dependencies

--- advancedform/src/App.js
+++ advancedform/src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios'
+import * as yup from 'yup'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+export default function App() {
+
+//initial form values
+const initialValues = {
+username: '',
+email: '',
+password: '',
+termsOfService: {acceptTerms: false,},
 }
 
-export default App;
+const initialDisabled = true
+
+//initial error values
+const initialErrors = {
+  username: '',
+  email: '',
+  password: '',
+}
+
+//slices of state
+const {formData, setFormData} = useState() 
+
+
+return(
+  <div></div>
+)
+
+
+}
